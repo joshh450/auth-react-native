497bc4faef62590502968aa4147a6361efebe62a
joshh450 <joshh450@yahoo.com>
2018-12-01 23:13:19 -0700

    added the firebase code and added in the card sections for the login card component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+                apiKey: "AIzaSyDp63M8RfG6p-j_e9eEpeQydm6F7uqzoPA",
+                authDomain: "auth-398e0.firebaseapp.com",
+                databaseURL: "https://auth-398e0.firebaseio.com",
+                projectId: "auth-398e0",
+                storageBucket: "auth-398e0.appspot.com",
+                messagingSenderId: "1018047001624"
+        });
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An app</Text>
+                <LoginForm/>
             </View>
         );
     }
