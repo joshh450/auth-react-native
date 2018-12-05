005df830c2ea5c6b556585ce7665606d086c6f45
joshh450 <joshh450@yahoo.com>
2018-12-05 16:23:07 -0700

    finished up the application by making the logout button do something on press

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
                 apiKey: "AIzaSyDp63M8RfG6p-j_e9eEpeQydm6F7uqzoPA",
@@ -14,13 +16,43 @@ class App extends Component {
                 storageBucket: "auth-398e0.appspot.com",
                 messagingSenderId: "1018047001624"
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </CardSection>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return (
+                <CardSection>
+                    <Spinner size="large" />
+                </CardSection>
+                );
+        }
+        
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm/>
+                {this.renderContent()}
             </View>
         );
     }
